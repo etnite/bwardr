728a74bfc9a4ffb12b2c24ad9860d4d7d09f9d66
bward8 <brian.phillip.ward@gmail.com>
2018-03-13 21:39:17 -0400

    Corrections to the two SNP PCA functions

--- R/eigstrat.R
+++ R/eigstrat.R
@@ -1,36 +1,37 @@
 #' Perform Eigenstrat marker PCA
 #' 
-#' @param geno A matrix of SNP data with SNPs in rows, genotypes in columns.
-#'   SNPs should be encoded in minor-allele dosage format, so that:
+#' @param geno A matrix of SNP data encoded in minor-allele dosage format:
 #'   2 = homozygous for minor allele
 #'   1 = heterozygous
 #'   0 = homozygous for major allele
 #'   NA = missing data
+#' @param snps Either "rows" to indicate that SNPs are in rows and individuals 
+#'   are in columns, or "cols" to indicate the SNPs are in columns and
+#'   individuals are in rows
 #' @return The Eigen decomposition of the input genotypic matrix
 #' @details This function performs principle component analysis on a matrix of
 #'   SNP data in the method of EIGENSTRAT (Price et al, 2006; doi:10.1038/ng1847)
 #' @importFrom stats cov
 #' @export
-eigstrat <- function(geno) {
-  col_names <- colnames(geno)
+eigstrat <- function(geno, snps = "rows") {
+  
+  ## Check matrix orientation
+  if (! snps %in% c("rows", "cols")) {
+    stop("Please specify whether SNPs are in 'rows' or 'cols'")
+  }
+  if (snps == "rows") { geno <- t(geno)}
   
   ## Center SNPs, set NA to 0
-  geno <- apply(geno, 1, function(x) {x - mean(x, na.rm = TRUE)})
+  geno <- scale(geno, scale = FALSE)
   geno[is.na(geno)] <- 0
   
-  ## apply() will transpose matrix if nrows < ncols
-  ## Usually won't happen with SNP data
-  if(identical(rownames(geno), col_names)) {
-    geno <- t(geno)
-  }
-  
   ## Normalize SNPs
-  p <- as.vector(1 + rowSums(geno))/(2 + 2*ncol(geno))
+  p <- as.vector(1 + colSums(geno))/(2 + 2*nrow(geno))
   denom <- sqrt(p * (1 - p))
-  geno <- t(t(geno) / denom)
+  geno <- geno/denom
   
   ## Calculate covariance matrix
-  cov_mat <- cov(geno)
+  cov_mat <- cov(t(geno))
   
   ## Perform Eigen decomposition
   return(eigen(cov_mat))
--- R/marker_pca.R
+++ R/marker_pca.R
@@ -6,23 +6,26 @@
 #'   1 = heterozygous
 #'   0 = homozygous for major allele
 #'   NA = missing data
+#' @param snps Either "rows" to indicate that SNPs are in rows and individuals 
+#'   are in columns, or "cols" to indicate the SNPs are in columns and
+#'   individuals are in rows
 #' @return The Eigen decomposition of the input genotypic matrix
 #' @details This function performs principle component analysis on a matrix of
 #'   SNP data. Note that there are a few transpositions required, as R would
 #'   typically not perform the centering/scaling/PCA in the correct orientation
 #' @importFrom stats prcomp
 #' @export
-marker_pca <- function(geno) {
-  col_names <- colnames(geno)
+marker_pca <- function(geno, snps = "rows") {
+
+  ## Check matrix orientation
+  if (! snps %in% c("rows", "cols")) {
+    stop("Please specify whether SNPs are in 'rows' or 'cols'")
+  }
+  if (snps == "cols") { geno <- t(geno)}
   
   ## Center/scale SNPs, set NA to 0
-  geno <- t(scale(t(geno)))
-  geno[is.na(geno)] <- 0
+  R <- (1/nrow(geno)) * (t(geno) %*% geno)
   
-  ## Perform the PCA
-  pca <- prcomp(geno, center = FALSE, scale. = FALSE)
-  eigvecs <- pca$x
-  eigvals <- pca$sdev^2
-  
-  return(list("vectors" = eigvecs, "values" = eigvals))
+  ## Perform eigen decomposition and return - that's it
+  return(eigen(R))
 }
