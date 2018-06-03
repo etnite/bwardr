0aa1dfc1ec8fe9568e7edaa5cec6f26c2d27dcaa
bward8 <brian.phillip.ward@gmail.com>
2018-06-02 20:17:36 -0400

    Changes to R/format_qtlmap_geno.R to take a bed file created iwth package 'gaston' as input

--- R/format_qtlmap_geno.R
+++ R/format_qtlmap_geno.R
@@ -1,14 +1,8 @@
 #' Generate files for QTL mapping
 #' 
-#' @param genomat A matrix or dataframe of SNP data with SNPs in rows and genotypes in
-#'   columns, encoded {0,1,2,NA}, where 0 indicates the homozygous state for one 
-#'   allele, 2 indicates the homozygous state for the alternate allele, 1
-#'   indicates the heterozygous state, and NA indicates missing data. The alleles
-#'   encoded by 0 and 2 for each SNP can be arbitrary. Row names should be
-#'   SNP identifiers, and column names should be genotype identifiers.
-#' @param map Dataframe containing map information for SNPs in genomat. This 
-#'   should contain the columns "SNP", "CHR", and "DIST" for the SNP ID, 
-#'   chromosome, and genetic distance (if known, or else all 0), respectively
+#' @param bed A binary ped (bed) object created with the package 'gaston',
+#'   likely either using the functions gaston::read.bed.matrix() or
+#'   gaston::read.vcf()
 #' @param par_a String identifying the column in the SNP matrix containing
 #'   data for the first parent.
 #' @param par_b String identifying the column in the SNP matrix containing 
@@ -19,80 +13,104 @@
 #'   in either parent.
 #' @param rm_miss Logical indicating whether to remove SNPs that are missing
 #'   in either parent.
+#' @param include_pars Logical indicating whether to include the parents in the
+#'   output data
 #' @param out_fmt String indicating the format of the output table - one of
 #'   either "icimapping" or "rqtl"
-#' @details It is strongly recommended that users utilize the read.vcf() function
-#'   in package 'gaston' to create a binary ped object from an input VCF genotypic
-#'   file. This binary ped object can then easily be converted into the required
-#'   input matrix using the command t(as.matrix(bed)). If "rqtl" is selected for
-#'   the output format, the function will output a table suitable for import to
-#'   R/qtl as the genotypic portion of the "csvs" format (which uses separate
-#'   genotypic and phenotypic tables)
+#' @details If "rqtl" output format is selected, a table suitable for use as the
+#'   genotypic portion of the separate, rotated (i.e. "csvsr") R/qtl input
+#'   format is produced.
 #' @export
-format_qtlmap_geno <- function(genomat, map, 
+format_qtlmap_geno <- function(bed, 
                                par_a, par_b, 
-                               rm_mono = TRUE, rm_het = TRUE, rm_miss = TRUE, 
+                               rm_mono = TRUE, rm_het = TRUE, rm_miss = TRUE,
+                               include_pars = TRUE,
                                out_fmt = "rqtl") {
   
-  ## Sanity checks
-  if (!out_fmt %in% c("icimapping", "rqtl")) {
+  ## Perform input sanity checks
+  if (!(par_a %in% bed@ped$id)) {
+    stop("Parent A is not present in supplied bed matrix")
+  }
+  
+  if (!(par_b %in% bed@ped$id)) {
+    stop("Parent B is not present in supplied bed matrix")
+  }
+    
+  if (!(is.logical(rm_mono) & 
+        is.logical(rm_het) & 
+        is.logical(rm_miss) & 
+        is.logical(include_pars))) {
+    stop("rm_mono, rm_het, rm_miss, and include_pars must be TRUE or FALSE")
+  }
+  
+  if (!(out_fmt %in% c("icimapping", "rqtl"))) {
     stop("Please select either 'icimapping' or 'rqtl' for out_fmt")
   }
   
-  if(colnames(map) != c("SNP", "CHR", "DIST")) {
-    stop("map dataframe should have column names 'SNP', 'CHR' and 'DIST'")
-  }
-  
-  ## First, 2 is assigned as the parent A allele, and 0 as the parent B allele
+  ## The integer 0 is assigned as the parent A allele, and 2 as the parent B allele
   ## This requires "flipping" some SNPs
-  ## After this, parent B should only have the 2 allele when SNP is monomorphic
-  genomat <- as.data.frame(genomat)
-  para_vec <- genomat[[par_a]]
-  genomat[para_vec == 0, ] <- genomat[para_vec == 0, ] - 2
+  ## After this, parent B should only have the 0 allele when a SNP is monomorphic
+  genomat <- as.data.frame(t(as.matrix(bed)))
+  flip <- rownames(genomat)[genomat[[par_a]] == 2]
+  genomat[flip, ] <- genomat[flip, ] - 2
   genomat <- abs(genomat)
+  
+  ## Flip the alleles in the bed object's snp info for good measure
+  a2 <- data.frame("id" = bed@snps$id, "A2" = bed@snps$A2, stringsAsFactors = FALSE)
+  bed@snps$A2[bed@snps$id %in% flip] <- bed@snps$A1[bed@snps$id %in% flip]
+  bed@snps$A1[bed@snps$id %in% flip] <- a2$A2[a2$id %in% flip]
   
   ## Reorder genotypes so that the two parents come first
   colorder <- setdiff(colnames(genomat), c(par_a, par_b))
   colorder <- c(par_a, par_b, colorder)
   genomat <- genomat[colorder]
   
-  ## Remove monomorphic SNPs if specified
+  ## Isolate the parents
+  parents <- data.frame("snp" = rownames(genomat),
+                        "a" = genomat[[par_a]],
+                        "b" = genomat[[par_b]],
+                        stringsAsFactors = FALSE)
+  
+  ## Find monomorphic SNPs if specified
+  drop_snps <- NULL
   if (rm_mono) {
-    genomat <- genomat[genomat[[par_a]] != genomat[[par_b]], ]
+    drop_snps <- union(drop_snps, parents$snp[parents$a == parents$b])
   }
   
-  ## Remove SNPs with either parent het if specified
+  ## Find SNPs with either parent het if specified
   if (rm_het) {
-    genomat <- genomat[genomat[[par_a]] != 1 & genomat[[par_b]] != 1, ]
+    drop_snps <- union(drop_snps, parents$snp[parents$a == 1 | parents$b == 1])
   }
   
-  ## Remove SNPs with either parent missing if specified
+  ## Find SNPs with either parent missing if specified
   if (rm_miss) {
-    genomat <- genomat[!is.na(genomat[[par_a]]) & !is.na(genomat[[par_b]]), ]
+    drop_snps <- union(drop_snps, parents$snp[is.na(parents$a) | is.na(parents$b)])
   }
   
-  ## Formatting for either ICIMapping or R/qtl output
+  ## Remove SNPs
+  genomat <- genomat[!rownames(genomat) %in% drop_snps, ]
+  bed@snps <- bed@snps[!bed@snps$id %in% drop_snps, ]
+  
+  ## Format for either ICIMapping or R/qtl output
+  genomat$id <- rownames(genomat)
   if (out_fmt == "icimapping") {
       genomat[is.na(genomat)] <- -1
+      id_df <- bed@snps[c("id", "chr", "dist", "pos", "A1", "A2")]
+      out_df <- merge(id_df, genomat, by = "id")
   } else {
-      genomat[genomat == 2] <- "A"
-      genomat[genomat == 0] <- "B"
+      genomat[genomat == 0] <- "A"
+      genomat[genomat == 2] <- "B"
       genomat[genomat == 1] <- "H"
+      genomat[is.na(genomat)] <- "-"
+      id_df <- bed@snps[c("id", "chr", "dist")]
+      out_df <- merge(id_df, genomat, by = "id")
+      names(out_df)[2:3] <- ""
   }
   
-  ## Merge in map data
-  genomat$SNP <- rownames(genomat)
-  genomat <- merge(map, genomat, by = "SNP")
-  
-  ## Additional formatting required for R/qtl output
-  if (out_fmt == "rqtl") {
-    rownames(genomat) <- genomat$SNP
-    drops <- c("SNP", "POS", "A1", "A2")
-    genomat <- genomat[, !(names(genomat) %in% drops)]
-    genomat <- as.data.frame(t(genomat), stringsAsFactors = FALSE)
-    genomat <- data.frame("id" = rownames(genomat), genomat, stringsAsFactors = FALSE)
-    genomat[1:2, 1] <- ""
+  ## Remove parents if specified
+  if (!include_pars) {
+    out_df[c(par_a, par_b)] <- NULL
   }
   
-  return(genomat)
+  return(out_df)
 }
