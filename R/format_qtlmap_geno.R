a34f62ca8ab26ef5b6c72ec2454c5bfc05a54a0e
bward8 <brian.phillip.ward@gmail.com>
2019-06-19 09:29:17 -0400

    Fixed bug in format_qtlmap_geno.R which caused incorrect allele assignment between parents when one parent had missing data or a het. call

--- R/format_qtlmap_geno.R
+++ R/format_qtlmap_geno.R
@@ -7,8 +7,6 @@
 #'   data for the first parent.
 #' @param par_b String identifying the column in the SNP matrix containing 
 #'   data for the second parent.
-#' @param rm_mono Logical indicating whether to remove SNPs that are monomorphic
-#'   between the two parents.
 #' @param rm_het Logical indicating whether to remove SNPs that are heterozygous
 #'   in either parent.
 #' @param rm_miss Logical indicating whether to remove SNPs that are missing
@@ -17,13 +15,20 @@
 #'   output data
 #' @param out_fmt String indicating the format of the output table - one of
 #'   either "icimapping" or "rqtl"
-#' @details If "rqtl" output format is selected, a table suitable for use as the
-#'   genotypic portion of the separate, rotated (i.e. "csvsr") R/qtl input
-#'   format is produced.
+#' @return list with components
+#'   \item{abh}{Dataframe in A/B/H format (see details)}
+#'   \item{flipped}{Character vector consisting of IDs of SNPs with flipped alleles}
+#' @details This function will always remove SNPs that are monomorphic between
+#'   the two parents, SNPs with missing data in both parents, and SNPs with 
+#'   with missing data in one parent and a heterozygous call in the other. The
+#'   user can optionally specify whether to remove SNPs with missing data for
+#'   either parent, or heterozygous calls in either parent. If "rqtl" output 
+#'   format is selected, a table suitable for use as the genotypic portion of 
+#'   the separate, rotated (i.e. "csvsr") R/qtl input format is produced.
 #' @export
 format_qtlmap_geno <- function(bed, 
                                par_a, par_b, 
-                               rm_mono = TRUE, rm_het = TRUE, rm_miss = TRUE,
+                               rm_het = TRUE, rm_miss = TRUE,
                                include_pars = TRUE,
                                out_fmt = "rqtl") {
   
@@ -36,60 +41,71 @@ format_qtlmap_geno <- function(bed,
     stop("Parent B is not present in supplied bed matrix")
   }
     
-  if (!(is.logical(rm_mono) & 
-        is.logical(rm_het) & 
+  if (!(is.logical(rm_het) & 
         is.logical(rm_miss) & 
         is.logical(include_pars))) {
-    stop("rm_mono, rm_het, rm_miss, and include_pars must be TRUE or FALSE")
+    stop("rm_het, rm_miss, and include_pars must be TRUE or FALSE")
   }
   
   if (!(out_fmt %in% c("icimapping", "rqtl"))) {
     stop("Please select either 'icimapping' or 'rqtl' for out_fmt")
   }
   
-  ## The integer 0 is assigned as the parent A allele, and 2 as the parent B allele
-  ## This requires "flipping" some SNPs
-  ## After this, parent B should only have the 0 allele when a SNP is monomorphic
+  ## Convert bed object to matrix
   genomat <- as.data.frame(t(gaston::as.matrix(bed)))
-  flip <- rownames(genomat[genomat[[par_a]] == 2, ])
-  genomat[flip, ] <- genomat[flip, ] - 2
-  genomat <- abs(genomat)
-  
-  ## Flip the alleles in the bed object's snp info for good measure
-  a2 <- data.frame("id" = bed@snps$id, "A2" = bed@snps$A2, stringsAsFactors = FALSE)
-  bed@snps$A2[bed@snps$id %in% flip] <- bed@snps$A1[bed@snps$id %in% flip]
-  bed@snps$A1[bed@snps$id %in% flip] <- a2$A2[a2$id %in% flip]
-  
-  ## Reorder genotypes so that the two parents come first
-  colorder <- setdiff(colnames(genomat), c(par_a, par_b))
-  colorder <- c(par_a, par_b, colorder)
-  genomat <- genomat[colorder]
   
   ## Isolate the parents
   parents <- data.frame("snp" = rownames(genomat),
                         "a" = genomat[[par_a]],
                         "b" = genomat[[par_b]],
                         stringsAsFactors = FALSE)
+  parents$sum <- parents$a + parents$b
   
-  ## Find monomorphic SNPs if specified
+  ### Select SNPs for removal ###
+  ## Always select SNPs that are monomorphic between parents
   drop_snps <- NULL
-  if (rm_mono) {
-    drop_snps <- union(drop_snps, parents$snp[parents$a == parents$b])
-  }
+  drop_snps <- union(drop_snps, parents$snp[parents$a == parents$b])
   
-  ## Find SNPs with either parent het if specified
+  ## Find SNPs with either parent het if specified; always remove SNPs with one
+  ## parent het, the other missing
   if (rm_het) {
     drop_snps <- union(drop_snps, parents$snp[parents$a == 1 | parents$b == 1])
+  } else {
+    drop_snps <- union(drop_snps, parents$snp[is.na(parents$a) & parents$b == 1])
+    drop_snps <- union(drop_snps, parents$snp[parents$a == 1 & is.na(parents$b)])
   }
   
-  ## Find SNPs with either parent missing if specified
+  ## Find SNPs with either parent missing if specified; always remove SNPs with
+  ## both parents missing
   if (rm_miss) {
     drop_snps <- union(drop_snps, parents$snp[is.na(parents$a) | is.na(parents$b)])
+  } else {
+    drop_snps <- union(drop_snps, parents$snp[is.na(parents$a) & is.na(parents$b)])
   }
   
-  ## Remove SNPs
+  ## Perform the SNP removal
   genomat <- genomat[!rownames(genomat) %in% drop_snps, ]
   bed@snps <- bed@snps[!bed@snps$id %in% drop_snps, ]
+  
+  ### Flip SNPs ###
+  
+  ## The integer 0 is assigned as the parent A allele, and 2 as the parent B allele
+  ## This requires "flipping" some SNPs (on average half of them)
+  flip <- parents$snp[parents$a == 2]
+  flip <- union(flip, parents$snp[is.na(parents$sum) & parents$b == 0])
+  flip <- union(flip, parents$snp[parents$sum == 1 & parents$b == 0])
+  flip <- flip[!is.na(flip)]
+  genomat[flip, ] <- genomat[flip, ] - 2
+  genomat <- abs(genomat)
+  
+  ## Remove parents if specified; otherwise reorder cols so parents appear first
+  if (include_pars) {
+    colorder <- setdiff(colnames(genomat), c(par_a, par_b))
+    colorder <- c(par_a, par_b, colorder)
+    genomat <- genomat[colorder]
+  } else {
+    genomat[c(par_a, par_b)] <- NULL
+  }
   
   ## Format for either ICIMapping or R/qtl output
   genomat$id <- rownames(genomat)
@@ -107,10 +123,6 @@ format_qtlmap_geno <- function(bed,
       names(out_df)[2:3] <- ""
   }
   
-  ## Remove parents if specified
-  if (!include_pars) {
-    out_df[c(par_a, par_b)] <- NULL
-  }
-  
-  return(out_df)
+  out_list <- list("abh" = out_df, "flipped" = flip)
+  return(out_list)
 }
