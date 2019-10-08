009ef284919f7dbebb2dc3d66352c362f648a3e9
bward8 <brian.phillip.ward@gmail.com>
2019-10-08 11:04:04 -0400

    Updated methods for calc_gmat.R; breaks compatibility with calls to past versions of function

--- R/calc_gmat.R
+++ R/calc_gmat.R
@@ -1,16 +1,17 @@
 #' Calculate Realized Genomic Relationship Matrices
 #'
 #' @param data Dataframe or matrix with individuals in rows and markers in columns.
-#'   Markers should beencoded in minor-allele dosage format
+#'   Markers should be encoded in minor-allele dosage format
 #'   (i.e. 0 = homozygous for major allele, 1 = heterozygous, 2 = homozygous
 #'   for minor allele).
-#' @param method Integer from 1 to 4 used to select the method to use for
-#' calculating the matrix. Available methods are:
-#'   1 - observed allele frequencies (GOF, VanRaden, 2008)
-#'   2 - weighted markers by recipricals of expected variance 
-#'       (GD, Forni et al., 2011)
-#'   3 - allele frequencies fixed at 0.5 (G05, Forni et al., 2011)
-#'   4 - allele frequencies fixed at mean for each locus (G05, Forni et al., 2011)
+#' @param method String to select the method to use for calculating the 
+#' relationship matrix. Available methods are:
+#' \enumerate{
+#'   \item "GOF" - observed allele frequencies (VanRaden, 2008)
+#'   \item "GD" - weighted markers by recipricals of expected variance (Forni et al., 2011)
+#'   \item "G05" - allele frequencies fixed at 0.5 (Forni et al., 2011)
+#'   \item "GM" - allele frequencies fixed at mean for each locus (Forni et al., 2011)
+#' }
 #' @return The full realized genetic relationship matrix (G)
 #' @details This function is adapted from an example from Chapter 11 of
 #'   Genetic Data Analysis for Plant and Animal Breeding, by Isik, Holland and
@@ -23,8 +24,8 @@
 calc_gmat <- function(data, method = 1) {
 
   ## Sanity check on supplied method integer
-  if (!method %in% c(1, 2, 3, 4)) {
-    stop("Please supply an integer from 1 to 4 to select G matrix calculation method")
+  if (!method %in% c("GOF", "GD", "G05", "GM")) {
+    stop("Please select 'GOF', 'GD', 'G05', or 'GM' for G matrix calculation method")
   }
   
   ## Create marker matrix encoded (-1, 0, 1)
@@ -32,10 +33,10 @@ calc_gmat <- function(data, method = 1) {
   M <- as.matrix(data) - 1
   
   ## Calculate or estimate p for each SNP, where p = (2 * minor allele freq)
-  if (method %in% c(1, 2)) {
+  if (method %in% c("GOF", "GD")) {
       p1 <- round((apply(M, 2, sum) + nrow(M))/(nrow(M) * 2), 3)
       p <- 2 * (p1 - 0.5)
-  } else if (method == 3) {
+  } else if (method == "G05") {
       p1 <- array(0.5, ncol(M))
       p <- p1
   } else {
@@ -48,7 +49,7 @@ calc_gmat <- function(data, method = 1) {
   Z <- as.matrix(M - P)
   
   ## Estimate G matrix from Z matrix
-  if (method %in% c(1, 3, 4)) {
+  if (method %in% c("GOF", "G05", "GM")) {
       b <- 1 - p1
       c <- p1 * b
       d <- 2 * (sum(c))
