1c22e248e58bbae0cee2f9da12051f4397fde10b
bward8 <brian.phillip.ward@gmail.com>
2020-02-16 20:58:46 -0500

    Added ability to use coop::covar to eigstrat.R

--- R/eigstrat.R
+++ R/eigstrat.R
@@ -19,6 +19,24 @@
 #'   SNP data in the method of EIGENSTRAT (Price et al, 2006; doi:10.1038/ng1847).
 #'   Note that while the user can select the number of eigenvectors to return,
 #'   the function always returns all the eigenvalues.
+#'   
+#'   The rate limiting step in this function is the calculation of a covariance
+#'   matrix. This can be sped up by using the covar() function from the 'coop'
+#'   package. The function will check if coop is installed, and use it if so,
+#'   otherwise falling back on the base cov() function. Note that the performance
+#'   of the coop package is dependent upon the BLAS library used on the system.
+#'   On Linux systems, (in this example Ubuntu-flavored), OpenBlas can be installed
+#'   and selected for use in a terminal:
+#'   
+#'   \code{sudo apt install libopenblas-dev}
+#'   \code{sudo update-alternatives --config libblas.so.3}
+#'   
+#'   On Windows and Mac, Microsoft R Open may be used, as it includes
+#'   the IntelMKL library.
+#'   
+#'   For large datasets, it is probably a good idea to use a higher-performance
+#'   method for performing PCA, such as the SNPRelate package, or else use
+#'   Eigensoft, GCTA, PLINK, etc.
 #' @importFrom stats cov
 #' @export
 eigstrat <- function(geno, snps = "rows", nvecs = 25) {
@@ -34,15 +52,15 @@ eigstrat <- function(geno, snps = "rows", nvecs = 25) {
   
   ## Normalize SNPs
   p_est <- as.vector(1 + colSums(geno, na.rm = TRUE))/(2 + 2*nrow(geno))
-  denom <- sqrt(p_est * (1 - p_est))
-  geno <- geno/denom
+  geno <- geno / sqrt(p_est * (1 - p_est))
   
-  ## Set NAs to 0; Calculate covariance matrix
+  ## Set NAs to 0; Perform Eigen decomposition on covariance matrix
   geno[is.na(geno)] <- 0
-  cov_mat <- cov(t(geno))
-  
-  ## Perform Eigen decomposition
-  eig <- eigen(cov_mat)
+  if (exists("tcovar", where = "package:coop", mode = "function")) {
+      eig <- eigen(coop::tcovar(geno))
+  } else {
+      eig <- eigen(cov(t(geno)))
+  }
   
   ## If nvecs is set to "all", give it a numeric val. equal to the number of
   ## eigenvectors
@@ -58,11 +76,8 @@ eigstrat <- function(geno, snps = "rows", nvecs = 25) {
   
   ## Create eigenvector data frame
   eigvecs <- as.data.frame(eig$vectors[, 1:nvecs])
-  colnames(eigvecs) <- paste("PC", c(1:ncol(eigvecs)), sep = "")
-  rownames(eigvecs) <- colnames(cov_mat)
+  colnames(eigvecs) <- paste0("PC", c(1:ncol(eigvecs)))
+  rownames(eigvecs) <- rownames(geno)
 
-  ## Assign eigenvalues to eigvals
-  eigvals <- eig$values
-  
-  return(list("eigvecs" = eigvecs, "eigvals" = eigvals))  
+  return(list("eigvecs" = eigvecs, "eigvals" = eig$values))  
 }
