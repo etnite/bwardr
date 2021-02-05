b85cdb453d616321abe9869afb6f141ba8cfa7f3
etnite <brian.phillip.ward@gmail.com>
2021-02-05 14:37:42 -0500

    Updated stand_str to recognize comma delimiters

--- R/stand_str.R
+++ R/stand_str.R
@@ -8,20 +8,22 @@
 #' @return character vector of same dimensions as input vector, but with all
 #'   elements updated to the new format
 #' @details This function somewhat crudely attempts to force character vectors
-#'   to a common format. It does this by converting everything to uppercase, and replacing
-#'   all common word delimiters (space, period, underscore, dash) with the selected one.
+#'   to a common format. It does this by converting everything to upper or lowercase, 
+#'   and replacing all common word delimiters (space, period, underscore, dash, 
+#'   comma) with the selected one.
 #'   Note that of the three, only underscores or periods will form syntactically-
 #'   valid R variable names (override by reading in tables with check.names = FALSE). 
-#'   However, underscores can cause problems with reading
-#'   VCFs in PLINK, as they will by default be interpreted as delimiters for
-#'   family and individual IDs
+#'   However, underscores can cause problems with reading VCFs in PLINK, as they 
+#'   will by default be interpreted as delimiters for family and individual IDs.
+#'   Choosing spaces or commas as the output delimiter is generally not recommended, as this
+#'   can cause issues in whitespace-delimited or comma-delimited text files, respectively.
 #' @export
 stand_str <- function(str_in, sep = "-", case = "upper") {
   
   ## Check for appropriate word delimiter selection
-  if(!sep %in% c(" ", "-", "_", ".", "")) {
+  if(!sep %in% c(" ", "-", "_", ".", "", ",")) {
     stop('Please select one of the following for the output word delimiter:
-         " " [space], ".", "-", "_", "" [no delimiter]')
+         " " [space], ".", "-", "_", ",", "" [no delimiter]')
   }
   
   ## Remove all non-ASCII characters
@@ -37,14 +39,14 @@ stand_str <- function(str_in, sep = "-", case = "upper") {
   ## Remove leading/trailing whitespace
   str_out <- trimws(str_out, which = "both")
   
-  ## Remove comment characters (hash sign)
+  ## Remove comment characters (hash/pound sign)
   str_out <- gsub("#", "", str_out, fixed = TRUE)
   
   ## Convert newlines to dashes
   str_out <- gsub("[\r\n]", "-", str_out)
   
   ## Convert all delimiters (except no delimiter) to dashes
-  str_out <- gsub("\\s|-|\\.|_", "-", str_out)
+  str_out <- gsub("\\s|-|,|\\.|_", "-", str_out)
   
   ## Replace repeated dashes with single
   str_out <- gsub("-+", "-", str_out)
